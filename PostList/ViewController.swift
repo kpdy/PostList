6c4c1a6102705349e8f1e886fe1d0b9f95a532e5
Kushal <kushal@doubleyolk.com>
2021-12-05 12:35:03 +0530

    Added the new xcode project.

---
+++ PostList/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  PostList
+//
+//  Created by DY on 05/12/21.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
